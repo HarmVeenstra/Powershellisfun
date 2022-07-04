736ad08b40b73013e37ffd89bd0875d05af9f0d3
Harm Veenstra <Harm.Veenstra@Gmail.com>
2022-07-04 15:44:21 +0200

    Removed the function and changed to normal script

---
+++ Message Of The Day/MOTD.ps1
@@ -0,0 +1,14 @@
+#Retrieve quote from goldenquotes.net
+#Get random numbers for the page number and quote number
+$randompage = Get-Random -Minimum 1 -Maximum 7
+$randomquote = Get-Random -Minimum 1 -Maximum 100
+try {
+    $page = Invoke-RestMethod -Uri "https://www.thegoldenquotes.net/best-100-public-domain-quotes-of-all-time-collection-0$($randompage)/"
+    $convertedpage = ConvertFrom-HTMLClass -Class "siteorigin-widget-tinymce" -Content $page -ErrorAction SilentlyContinue
+    $quote = "Message Of The Day:`n" + $($convertedpage)[$($randomquote)].substring(2)
+    return $quote
+}
+#Show an error is there is an issue
+catch {
+    Write-Host "Error retrieving MOTD, please try again later..." -ForegroundColor Red
+}
