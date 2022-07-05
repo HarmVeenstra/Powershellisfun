5b983d945204a35448b51ef03a778ad2ab82ab83
Harm Veenstra <Harm.Veenstra@Gmail.com>
2022-07-05 15:07:13 +0200

    Added check for PSParseHTML module

--- Message Of The Day/MOTD.ps1
+++ Message Of The Day/MOTD.ps1
@@ -2,6 +2,14 @@
 #Get random numbers for the page number and quote number
 $randompage = Get-Random -Minimum 1 -Maximum 7
 $randomquote = Get-Random -Minimum 1 -Maximum 100
+
+#Check if the module PSParseHTML is installed and install
+#the module if it's not installed
+if (-not (Get-Command ConvertFrom-HTMLClass -ErrorAction SilentlyContinue)) {
+    Install-Module PSParseHTML -SkipPublisherCheck -Force:$true -Confirm:$false
+}
+
+#Get a random quote and display it
 try {
     $page = Invoke-RestMethod -Uri "https://www.thegoldenquotes.net/best-100-public-domain-quotes-of-all-time-collection-0$($randompage)/"
     $convertedpage = ConvertFrom-HTMLClass -Class "siteorigin-widget-tinymce" -Content $page -ErrorAction SilentlyContinue
