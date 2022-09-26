3abb77749d478f8f4b4894223566916def28d5e4
Harm Veenstra <Harm.Veenstra@Gmail.com>
2022-09-26 19:18:58 +0200

    Changed to Write-Warning and formatting

--- Create A Hyper-V Running Evaluation Versions Of Windows Server/Create_Eval_Hyper_V_VM.ps1
+++ Create A Hyper-V Running Evaluation Versions Of Windows Server/Create_Eval_Hyper_V_VM.ps1
@@ -5,10 +5,10 @@ $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
  
 #Detect if Hyper-V is installed
 if ((Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online).State -ne 'Enabled') {
-    Write-host 'Hyper-V Role and/or required PowerShell module is not installed, please install before running this script...' -ForegroundColor Red
+    Write-Warning ("Hyper-V Role and/or required PowerShell module is not installed, please install before running this script...")
 }
 else {
-    Write-host 'Hyper-V Role is installed, continuing...' -ForegroundColor Green
+    Write-Host ("Hyper-V Role is installed, continuing...") -ForegroundColor Green
 }
  
 #Retrieve all Server Operating System VHD links from the Microsoft Evaluation Center
@@ -33,7 +33,7 @@ foreach ($url in $urls) {
         }
         $count = $DownloadLinks.href.Count
         $totalcount += $count
-        Write-host "Processing $($url), Found $($Count) Download(s)..." -ForegroundColor Green
+        Write-host ("Processing {0}, Found {1} Download(s)..." -f $url, $count) -ForegroundColor Green
         foreach ($DownloadLink in $DownloadLinks) {
             $found = [PSCustomObject]@{
                 Title  = $content.ParsedHtml.title.Split('|')[0]
@@ -46,7 +46,7 @@ foreach ($url in $urls) {
         }
     }
     catch {
-        Write-host $url is not accessible -ForegroundColor Red
+        Write-Warning ("{0} is not accessible" -f $url)
         break
     }
 }
@@ -54,14 +54,14 @@ foreach ($url in $urls) {
 #Select VHD from the list
 $VHD = $totalfound | Out-GridView -OutputMode Single -Title 'Please select the VHD file to use and click OK' | Select-Object Name, Link
 if (($VHD.Name).Count -ne '1') {
-    Write-host 'No VHD file selected, script aborted...' -ForegroundColor Red
+    Write-Warning ("No VHD file selected, script aborted...")
     break
 }
  
 #Set VM Parameters
 $VMname = Read-Host 'Please enter the name of the VM to be created, for example W2K22SRV'
 if ((Get-VM -Name $VMname -ErrorAction SilentlyContinue).count -ge 1) {
-    write-host "$($VMname) already exists on this system, aborting..." -ForegroundColor Red
+    Write-Warning ("{0} already exists on this system, aborting..." -f $VMname)
     break
 } 
 $VMCores = Read-Host 'Please enter the amount of cores, for example 2'
@@ -69,7 +69,7 @@ $VMCores = Read-Host 'Please enter the amount of cores, for example 2'
 $VMdir = (get-vmhost).VirtualMachinePath + $VMname
 $SwitchName = Get-VMSwitch | Out-GridView -OutputMode Single -Title 'Please select the VM Switch and click OK' | Select-Object Name
 if (($SwitchName.Name).Count -ne '1') {
-    Write-host 'No Virtual Switch selected, script aborted...' -ForegroundColor Red
+    Write-Warning ("No Virtual Switch selected, script aborted...")
     break
 }
  
@@ -78,17 +78,17 @@ try {
     New-Item -ItemType Directory -Path $VMdir -Force:$true -ErrorAction SilentlyContinue | Out-Null
 }
 catch {
-    write-host "Couldn't create $($VMdir) folder, please check VM Name for illegal characters or permissions on folder..." -ForegroundColor Red
+    Write-Warning ("Couldn't create {0} folder, please check VM Name for illegal characters or permissions on folder..." -f $VMdir)
     break
 }
 finally {
     if (test-path -Path $VMdir -ErrorAction SilentlyContinue) { 
-        Write-Host "Using $($VMdir) as Virtual Machine location... " -ForegroundColor Green
+        Write-Host ("Using {0} as Virtual Machine location..." -f $VMdir) -ForegroundColor Green
     }
 }
  
 #Download VHD file to the VirtualMachinePath\VMname
-write-host "Downloading $($vhd.Name) to $($VMdir)..." -ForegroundColor Green
+write-host ("Downloading {0} to {1}..." -f $vhd.Name, $VMdir) -ForegroundColor Green
 $VHDFile = "$($VMdir)\$($VMname)" + ".vhd"
 $VMPath = (Get-VMHost).VirtualMachinePath + '\'
 Invoke-WebRequest -Uri $vhd.Link -OutFile $VHDFile
@@ -98,25 +98,25 @@ try {
     New-VM -Name $VMname -SwitchName $SwitchName.Name -Path $VMPath -Generation 1 -NoVHD:$true -Confirm:$false -ErrorAction Stop | Out-Null  
 }
 catch {
-    Write-Host "Error creating $($VMname), please check logs and make sure $($VMname) doesn't already exist..." -ForegroundColor Red
+    Write-Warning ("Error creating {0}, please check logs and make sure {1} doesn't already exist..." -f $VMname, $VMname)
 }
 finally {
     if (Get-VM -Name $VMname -ErrorAction SilentlyContinue | Out-Null) {
-        write-host "Created $($VMname)..." -ForegroundColor Green
+        write-host ("Created {0}..." -f $VMname) -ForegroundColor Green
     }
 }
  
 #Configure settings on the VM, CPU/Memory/Disk/BootOrder
 try {
-    Write-Host "Configuring settings on $($VMname)..." -ForegroundColor Green
+    Write-Host ("Configuring settings on {0}..." -f $VMname) -ForegroundColor Green
     Set-VM -name $VMname -ProcessorCount $VMCores -DynamicMemory -MemoryMinimumBytes 64MB -MemoryMaximumBytes $VMRAM -MemoryStartupBytes 512MB  -ErrorAction SilentlyContinue | Out-Null 
     Add-VMHardDiskDrive -VMName $VMname -Path $VHDFile -ControllerType IDE -ErrorAction SilentlyContinue | Out-Null
 }
 catch {
-    Write-Host "Error setting VM parameters, check $($VMname) settings..." -ForegroundColor Red
+    Write-Warning ("Error setting VM parameters, check {0} settings..." -f $VMname)
     break
 }
  
 #The end, stop stopwatch and display the time that it took to deploy
 $stopwatch.Stop()
-Write-Host "Done, the deployment took $($stopwatch.Elapsed.Hours) hours, $($stopwatch.Elapsed.Minutes) minutes and $($stopwatch.Elapsed.Seconds) seconds" -ForegroundColor Green
+Write-Host ("Done, the deployment took {0} hours, {1} minutes and {2} seconds" -f $stopwatch.Elapsed.Hours, $stopwatch.Elapsed.Minutes, $stopwatch.Elapsed.Seconds) -ForegroundColor Green
