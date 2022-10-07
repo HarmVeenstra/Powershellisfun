d8eea13bffe7303caa8ad18be011d5bd653f4aff
Harm Veenstra <Harm.Veenstra@Gmail.com>
2022-10-07 23:48:38 +0200

    Change pscustomobject

--- Read RSS Feeds/Get_RSS_Feed.ps1
+++ Read RSS Feeds/Get_RSS_Feed.ps1
@@ -1,12 +1,10 @@
 $numberofitems = '5'
-$total = @()
-foreach ($item in Invoke-RestMethod -Uri http://powershellisfun.com/feed ) {
-    $article = [PSCustomObject]@{
+$total = foreach ($item in Invoke-RestMethod -Uri http://powershellisfun.com/feed ) {
+    [PSCustomObject]@{
         'Publication date' = $item.pubDate
         Title              = $item.Title
         Link               = $item.Link
     }
-    $total += $article
 }
  
 $total | Sort-Object { $_."Publication Date" -as [datetime] } |  Select-Object -Last $numberofitems
