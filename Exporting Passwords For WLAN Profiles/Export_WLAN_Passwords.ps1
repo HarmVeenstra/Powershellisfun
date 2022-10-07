87186bb50ea05990b507037a35df70225cf47b09
Harm Veenstra <Harm.Veenstra@Gmail.com>
2022-10-08 00:17:33 +0200

    Change pscustomobject

--- Exporting Passwords For WLAN Profiles/Export_WLAN_Passwords.ps1
+++ Exporting Passwords For WLAN Profiles/Export_WLAN_Passwords.ps1
@@ -1,33 +1,28 @@
-#Create an empty $passwords array which will contain all information
-$passwords = @()
- 
 #Set the output location for .csv file
 $output = 'd:\temp\ssid_passwords.csv'
  
 #Retrieve all WLAN profiles, loop through them and try to get the passsword
 $wlanprofiles = (netsh wlan show profiles) | select-string ': '
 if ($null -ne $wlanprofiles) {
-    foreach ($wlanprofile in $wlanprofiles | Sort-Object) {
+    $passwords = foreach ($wlanprofile in $wlanprofiles | Sort-Object) {
         try {
             $profile_information = netsh wlan show profile name="$($wlanprofile.ToString().Split(':')[1].SubString(1))" key=clear
             write-host ("Retrieving password for SSID {0}" -f $wlanprofile.ToString().Split(':')[1].SubString(1)) -ForegroundColor Green
-            $password = [PSCustomObject]@{
+            [PSCustomObject]@{
                 'SSID'                = $wlanprofile.ToString().Split(':')[1].SubString(1)
                 'Authentication Type' = ($profile_information | select-string 'Authentication' | Select-Object -First 1).Tostring().Split(':')[1].Substring(1)
                 'Password'            = ($profile_information | select-string 'Key Content').Tostring().Split(':')[1].Substring(1)
             }
-            $passwords += $password
         }
         catch {
             #If retrieving the password fails, add the reason why to $passwords in the password field
             $authenticationtype = ($profile_information | select-string 'Authentication' | Select-Object -First 1).Tostring().Split(':')[1].Substring(1)
             Write-Warning ("Could not retrieve password for SSID {0}, check {1}" -f $wlanprofile.ToString().Split(':')[1].SubString(1), $output)
-            $password = [PSCustomObject]@{
+            [PSCustomObject]@{
                 'SSID'                = $wlanprofile.ToString().Split(':')[1].SubString(1)
                 'Authentication Type' = ($profile_information | select-string 'Authentication' | Select-Object -First 1).Tostring().Split(':')[1].Substring(1)
                 'Password'            = "Could not retrieve password for the SSID because it's an $($authenticationtype) network"
             }
-            $passwords += $password
         }
     }
      
