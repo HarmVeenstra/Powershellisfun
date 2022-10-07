fcdd10106745fe5474353d7b8bcb2ca64c39f156
Harm Veenstra <Harm.Veenstra@Gmail.com>
2022-10-08 00:38:57 +0200

    Fixed, wrong script contents

--- Adding Mobile Phone Authentication Method For Azure AD MFA/Add_Mobile_Phone_Auth_Method.ps1
+++ Adding Mobile Phone Authentication Method For Azure AD MFA/Add_Mobile_Phone_Auth_Method.ps1
@@ -1,35 +1,26 @@
-#Set the output location for .csv file
-$output = 'd:\temp\ssid_passwords.csv'
- 
-#Retrieve all WLAN profiles, loop through them and try to get the passsword
-$wlanprofiles = (netsh wlan show profiles) | select-string ': '
-if ($null -ne $wlanprofiles) {
-    $passwords = foreach ($wlanprofile in $wlanprofiles | Sort-Object) {
-        try {
-            $profile_information = netsh wlan show profile name="$($wlanprofile.ToString().Split(':')[1].SubString(1))" key=clear
-            write-host ("Retrieving password for SSID {0}" -f $wlanprofile.ToString().Split(':')[1].SubString(1)) -ForegroundColor Green
-            [PSCustomObject]@{
-                'SSID'                = $wlanprofile.ToString().Split(':')[1].SubString(1)
-                'Authentication Type' = ($profile_information | select-string 'Authentication' | Select-Object -First 1).Tostring().Split(':')[1].Substring(1)
-                'Password'            = ($profile_information | select-string 'Key Content').Tostring().Split(':')[1].Substring(1)
-            }
+#Used https://docs.microsoft.com/en-us/azure/active-directory/authentication/howto-mfa-userdevicesettings for the method of setting a mobile number for MFA 
+# Install the needed Microsoft.Graph modules
+Install-module Microsoft.Graph.Authentication, Microsoft.Graph.Identity.Signins, Microsoft.Graph.Users -ErrorAction SilentlyContinue
+
+# Connect to tenant, make sure your account has enough permissons in Microsoft Graph
+# Use https://developer.microsoft.com/en-us/graph/graph-explorer to grant permissions 
+# in https://graph.microsoft.com/v1.0/users/objectid/authentication/microsoftAuthenticatorMethods
+# and https://graph.microsoft.com/v1.0/users/
+Connect-MgGraph -Scopes UserAuthenticationMethod.ReadWrite.All, User.Read.All
+Select-MgProfile -Name beta
+
+# Loop through the users (No guest accounts) who have registered MFA without a recovery phonenumber
+# and add it if a mobile phone number is present in Azure AD for the user.
+# The ID 28c10230-6103-485e-b985-444c60001490 is filtered because it's the standard Password.
+foreach ($user in Get-MgUser -All | Where-Object UserPrincipalName -NotMatch '#EXT#') {
+    if ($null -ne (Get-MgUserAuthenticationMethod -UserId $user.UserPrincipalName | Where-Object ID -ne 28c10230-6103-485e-b985-444c60001490) `
+            -and $null -eq (Get-MgUserAuthenticationPhoneMethod -UserId $user.UserPrincipalName)) {
+        if ($null -ne $user.MobilePhone) {
+            Write-Host "$($user.UserPrincipalName) has registered MFA but has no mobile phone Authentication Method, adding $($user.MobilePhone) now" -ForegroundColor Green
+            New-MgUserAuthenticationPhoneMethod -UserId $user.UserPrincipalName -phoneType "Mobile" -phoneNumber $user.MobilePhone | Out-Null           
         }
-        catch {
-            #If retrieving the password fails, add the reason why to $passwords in the password field
-            $authenticationtype = ($profile_information | select-string 'Authentication' | Select-Object -First 1).Tostring().Split(':')[1].Substring(1)
-            Write-Warning ("Could not retrieve password for SSID {0}, check {1}" -f $wlanprofile.ToString().Split(':')[1].SubString(1), $output)
-            [PSCustomObject]@{
-                'SSID'                = $wlanprofile.ToString().Split(':')[1].SubString(1)
-                'Authentication Type' = ($profile_information | select-string 'Authentication' | Select-Object -First 1).Tostring().Split(':')[1].Substring(1)
-                'Password'            = "Could not retrieve password for the SSID because it's an $($authenticationtype) network"
-            }
+        else {
+            write-host "$($user.UserPrincipalName) has MFA configured without mobile phone number Authentication Method but has no known mobile phone number to add, skipping..." -ForegroundColor Red
         }
     }
-     
-    #Export to $output path and open Excel (Or prompt to associate the .csv file, choose Notepad/Wordpad etc. to view the contents)
-    $passwords | export-csv -NoTypeInformation -Encoding UTF8 -Delimiter ';' -Path $output
-    Invoke-Item $output
-}
-else {
-    Write-Warning ("No WLAN profiles found, please check if {0} has a Wi-Fi adapter or any saved networks" -f $env:COMPUTERNAME)
 }
