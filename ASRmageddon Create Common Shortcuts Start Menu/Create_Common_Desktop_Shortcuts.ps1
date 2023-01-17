5f7def6aa4eb88850e6940e0f4518bdac94075db
Harm Veenstra <Harm.Veenstra@Gmail.com>
2023-01-17 11:12:52 +0100

    Initial

--- ASRmageddon Create Common Shortcuts Start Menu/Create_Common_Desktop_Shortcuts.ps1
+++ ASRmageddon Create Common Shortcuts Start Menu/Create_Common_Desktop_Shortcuts.ps1
@@ -1,20 +1,13 @@
 $programs = @{
-    "Access"                   = "C:\Program Files\Microsoft Office\root\Office16\MSACCESS.EXE"
     "Adobe Acrobat"            = "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
     "Excel"                    = "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"
-    "Firefox Private Browsing" = "C:\Program Files\Mozilla Firefox\private_browsing.exe"
     "Firefox"                  = "C:\Program Files\Mozilla Firefox\firefox.exe"
     "Google Chrome"            = "C:\Program Files\Google\Chrome\Application\chrome.exe"
     "Microsoft Edge"           = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-    "Notepad++"                = "C:\Program Files\Notepad++\notepad++.exe"
     "OneNote"                  = "C:\Program Files\Microsoft Office\root\Office16\ONENOTE.EXE"
     "Outlook"                  = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
-    "PowerPoint"               = "C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE"
-    "Project"                  = "C:\Program Files\Microsoft Office\root\Office16\WINPROJ.EXE"
-    "Publisher"                = "C:\Program Files\Microsoft Office\root\Office16\MSPUB.EXE"
     "Remote Desktop"           = "C:\Program Files\Remote Desktop\msrdcw.exe"
     "TeamViewer"               = "C:\Program Files\TeamViewer\TeamViewer.exe"
-    "Visio"                    = "C:\Program Files\Microsoft Office\root\Office16\VISIO.EXE"
     "Word"                     = "C:\Program Files\Microsoft Office\root\Office16\WINWORD.exe"
 }
 
