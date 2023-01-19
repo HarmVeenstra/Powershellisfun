6bece0007a0761704ef375972d8f0ccb01da61dc
Harm Veenstra <Harm.Veenstra@Gmail.com>
2023-01-19 11:15:12 +0100

    Changed pnputil to not use path detection

--- Adding printer drivers with Endpoint Manager/install_printerdrivers.ps1
+++ Adding printer drivers with Endpoint Manager/install_printerdrivers.ps1
@@ -1,11 +1,6 @@
 $drivers = Import-Csv .\Drivers.csv -Delimiter ','
 foreach ($driver in $drivers) {
-    if (Test-Path C:\Windows\Sysnative\pnputil.exe) {
-        C:\Windows\Sysnative\pnputil.exe -a $driver.Path
-    }
-    else {
-        C:\Windows\Sytem32\pnputil.exe -a $driver.Path
-    }
+    pnputil.exe -a $driver.Path
     Start-Sleep -Seconds 5
     Add-PrinterDriver -Name $driver.name
 }
