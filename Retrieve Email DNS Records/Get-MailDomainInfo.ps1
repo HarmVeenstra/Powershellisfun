d2fbf54973e273566b908c21faa019d497ba98e1
Harm Veenstra <Harm.Veenstra@Gmail.com>
2023-08-03 11:05:31 +0200

    Updated to allow checking multiple domains at once

--- Retrieve Email DNS Records/Get-MailDomainInfo.ps1
+++ Retrieve Email DNS Records/Get-MailDomainInfo.ps1
@@ -1,6 +1,6 @@
 function Get-MailDomainInfo {
     param(
-        [parameter(Mandatory = $true)][string]$DomainName,
+        [parameter(Mandatory = $true)][string[]]$DomainName,
         [parameter(Mandatory = $false)][string]$DNSserver
     )
      
@@ -8,60 +8,63 @@ function Get-MailDomainInfo {
     if (-not ($DNSserver)) {
         $DNSserver = '1.1.1.1'
     }
+
+    $info = foreach ($domain in $DomainName) {
  
-    #Retrieve all mail DNS records
-    $autodiscoverA = (Resolve-DnsName -Name "autodiscover.$($domainname)" -Type A -Server $DNSserver -ErrorAction SilentlyContinue).IPAddress
-    $autodiscoverCNAME = (Resolve-DnsName -Name "autodiscover.$($domainname)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue).NameHost
-    $dkim1 = Resolve-DnsName -Name "selector1._domainkey.$($domainname)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue
-    $dkim2 = Resolve-DnsName -Name "selector2._domainkey.$($domainname)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue
-    $domain = Resolve-DnsName -Name $DomainName -Server $DNSserver -ErrorAction SilentlyContinue
-    $dmarc = (Resolve-DnsName -Name "_dmarc.$($DomainName)" -Type TXT -Server $DNSserver -ErrorAction SilentlyContinue | Where-Object Strings -Match 'DMARC').Strings
-    $mx = (Resolve-DnsName -Name $DomainName -Type MX -Server $DNSserver -ErrorAction SilentlyContinue).NameExchange
-    $spf = (Resolve-DnsName -Name $DomainName -Type TXT -Server $DNSserver -ErrorAction SilentlyContinue | Where-Object Strings -Match 'v=spf').Strings
+        #Retrieve all mail DNS records
+        $autodiscoverA = (Resolve-DnsName -Name "autodiscover.$($domain)" -Type A -Server $DNSserver -ErrorAction SilentlyContinue).IPAddress
+        $autodiscoverCNAME = (Resolve-DnsName -Name "autodiscover.$($domain)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue).NameHost
+        $dkim1 = Resolve-DnsName -Name "selector1._domainkey.$($domain)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue
+        $dkim2 = Resolve-DnsName -Name "selector2._domainkey.$($domain)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue
+        $domaincheck = Resolve-DnsName -Name $domain -Server $DNSserver -ErrorAction SilentlyContinue
+        $dmarc = (Resolve-DnsName -Name "_dmarc.$($domain)" -Type TXT -Server $DNSserver -ErrorAction SilentlyContinue | Where-Object Strings -Match 'DMARC').Strings
+        $mx = (Resolve-DnsName -Name $domain -Type MX -Server $DNSserver -ErrorAction SilentlyContinue).NameExchange
+        $spf = (Resolve-DnsName -Name $domain -Type TXT -Server $DNSserver -ErrorAction SilentlyContinue | Where-Object Strings -Match 'v=spf').Strings
  
-    #Set variables to Not enabled or found if they can't be retrieved
-    #and stop script if domainname is not valid 
-    $errorfinding = 'Not enabled'
-    if ($null -eq $domain) {
-        Write-Warning ("{0} not found" -f $DomainName)
-        return
-    }
+        #Set variables to Not enabled or found if they can't be retrieved
+        #and stop script if domaincheck is not valid 
+        $errorfinding = 'Not enabled'
+        if ($null -eq $domaincheck) {
+            Write-Warning ("{0} not found" -f $domaincheck)
+            return
+        }
  
-    if ($null -eq $dkim1 -and $null -eq $dkim2) {
-        $dkim = $errorfinding
-    }
-    else {
-        $dkim = "$($dkim1.Name) , $($dkim2.Name)"
-    }
+        if ($null -eq $dkim1 -and $null -eq $dkim2) {
+            $dkim = $errorfinding
+        }
+        else {
+            $dkim = "$($dkim1.Name) , $($dkim2.Name)"
+        }
  
-    if ($null -eq $dmarc) {
-        $dmarc = $errorfinding
-    }
+        if ($null -eq $dmarc) {
+            $dmarc = $errorfinding
+        }
  
-    if ($null -eq $mx) {
-        $mx = $errorfinding
-    }
+        if ($null -eq $mx) {
+            $mx = $errorfinding
+        }
  
-    if ($null -eq $spf) {
-        $spf = $errorfinding
-    }
+        if ($null -eq $spf) {
+            $spf = $errorfinding
+        }
  
-    if (($autodiscoverA).count -gt 1) {
-        $autodiscoverA = $errorfinding
-    }
+        if (($autodiscoverA).count -gt 1) {
+            $autodiscoverA = $errorfinding
+        }
  
-    if ($null -eq $autodiscoverCNAME) {
-        $autodiscoverCNAME = $errorfinding
-    }
+        if ($null -eq $autodiscoverCNAME) {
+            $autodiscoverCNAME = $errorfinding
+        }
  
-    $info = [PSCustomObject]@{
-        'Domain Name'             = $DomainName
-        'Autodiscover IP-Address' = $autodiscoverA
-        'Autodiscover CNAME '     = $autodiscoverCNAME
-        'DKIM Record'             = $dkim
-        'DMARC Record'            = "$($dmarc)"
-        'MX Record(s)'            = $mx -join ', '
-        'SPF Record'              = "$($spf)"
+        [PSCustomObject]@{
+            'Domain Name'             = $domain
+            'Autodiscover IP-Address' = $autodiscoverA
+            'Autodiscover CNAME '     = $autodiscoverCNAME
+            'DKIM Record'             = $dkim
+            'DMARC Record'            = "$($dmarc)"
+            'MX Record(s)'            = $mx -join ', '
+            'SPF Record'              = "$($spf)"
+        }
     }
          
     return $info
