37fbb04a5987386fa9b786c8f5529391e07def08
Harm Veenstra <Harm.Veenstra@Gmail.com>
2023-10-01 17:16:41 +0200

    Performance update/Changed version checking method

--- Check for PowerShell modules updates/Find-ModuleUpdates.ps1
+++ Check for PowerShell modules updates/Find-ModuleUpdates.ps1
@@ -7,35 +7,47 @@ param (
 Write-Host ("Retrieving installed PowerShell modules") -ForegroundColor Green
 $InstalledModules = Get-InstalledModule -Name $NameFilter -ErrorAction SilentlyContinue
 
-#Start checking for updates if $InstalledModules is greater than 1
+#Retrieve current versions of modules (63 at a time because of PSGallery limit) if $InstalledModules is greater than 0
 if ($InstalledModules.Count -gt 0) {
-    $number = 1
-    #Loop through all modules and check for newer versions and add those to $total
-    Write-Host ("Checking for updated versions") -ForegroundColor Green
-    $total = foreach ($module in $InstalledModules) {
-        Write-Progress ("[{0}/{1} Checking module {2}" -f $number, $InstalledModules.count, $module.name)
-        try {
-            $PsgalleryModule = Find-Module -Name $Module.Name -ErrorAction Stop
-            if ([version]$module.version -lt [version]$PsgalleryModule.version) {
-                [PSCustomObject]@{
-                    Repository          = $module.Repository
-                    'Module name'       = $module.Name
-                    'Installed version' = $module.Version
-                    'Latest version'    = $PsgalleryModule.version
-                    'Published on'      = $PsgalleryModule.PublishedDate
-                }
-            }
-        }
-        catch {
-            Write-Warning ("Could not find module {0}" -f $module.Name)
-        }
-        $number++
+    $startnumber = 0
+    $endnumber = 62
+    $onlineversions = $null
+    while ($InstalledModules.Count -gt $onlineversions.Count) {
+        Write-Host ("Checking online versions for installed modules [{0}..{1}/{2}]" -f $startnumber, $endnumber, $InstalledModules.Count) -ForegroundColor Green
+        $currentversions = Find-Module -Name $InstalledModules.name[$startnumber..$endnumber]
+        $startnumber = $startnumber + 63
+        $endnumber = $endnumber + 63
+        $onlineversions = $onlineversions + $currentversions
     }
 }
 else {
-    Write-Warning ("No modules were found to check for updates, please check yourNameFilter. Exiting...")
+    Write-Warning ("No modules were found to check for updates, please check your NameFilter. Exiting...")
     return
 }
+
+#Loop through all modules and check for newer versions and add those to $total
+$number = 1
+Write-Host ("Checking for updated versions") -ForegroundColor Green
+$total = foreach ($module in $InstalledModules) {
+    Write-Progress ("[{0}/{1} Checking module {2}" -f $number, $InstalledModules.count, $module.name)
+    try {
+        $PsgalleryModule = $onlineversions | Where-Object name -eq $module.Name
+        if ([version]$module.version -lt [version]$PsgalleryModule.version) {
+            [PSCustomObject]@{
+                Repository          = $module.Repository
+                'Module name'       = $module.Name
+                'Installed version' = $module.Version
+                'Latest version'    = $PsgalleryModule.version
+                'Published on'      = $PsgalleryModule.PublishedDate
+            }
+        }
+    }
+    catch {
+        Write-Warning ("Could not find module {0}" -f $module.Name)
+    }
+    $number++
+}
+
 
 #Output $total to display updates for installed modules if any
 if ($total.Count -gt 0) {
