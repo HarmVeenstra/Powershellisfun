5529510ed332a34c4179d62c5b0e7e627bb8d5ba
Harm Veenstra <Harm.Veenstra@Gmail.com>
2023-11-29 11:09:06 +0100

    Changed vhdx seach method

--- Compact Hyper-V VHDX files/Compact-VHDX.ps1
+++ Compact Hyper-V VHDX files/Compact-VHDX.ps1
@@ -30,10 +30,10 @@ function Compact-VHDX {
     
     #Gather all VHDXs from the VMs (or VM is $VMName was specified which don't have a parent/snapshot
     if (-not ($VMName)) {
-        $vhds = Hyper-V\Get-VM | Hyper-V\Get-VMHardDiskDrive | Where-Object Path -Match '.vhdx' | Sort-Object VMName, Path
+        $vhds = Hyper-V\Get-VM | Hyper-V\Get-VMHardDiskDrive | Where-Object Path -Like '*.vhdx' | Sort-Object VMName, Path
     }
     else {
-        $vhds = Hyper-V\Get-VM $VMName | Hyper-V\Get-VMHardDiskDrive | Where-Object Path -Match '.vhdx' | Sort-Object Path
+        $vhds = Hyper-V\Get-VM $VMName | Hyper-V\Get-VMHardDiskDrive | Where-Object Path -Like '*.vhdx' | Sort-Object Path
     }
 
     if ($null -eq $vhds) {
@@ -53,36 +53,34 @@ function Compact-VHDX {
     #Compress all files
     foreach ($vhd in $vhds) {
         if (-not (Hyper-V\Get-VM $vhd.VMName  | Where-Object State -eq Running)) {
-            if ((Hyper-V\Get-VHD  $vhd.path).VhdType -eq 'Dynamic') {
-                Write-Host ("`nProcessing {0} from VM {1}..." -f $vhd.Path, $vhd.VMName) -ForegroundColor Gray
-                try {
-                    Hyper-V\Mount-VHD -Path $vhd.Path -ReadOnly -ErrorAction Stop
-                    Write-Host "Mounting VHDX" -ForegroundColor Green
-                }
-                catch {
-                    Write-Warning ("Error mounting {0}, please check access or if file is locked..." -f $vhd.Path )
-                    continue
-                }
-
-                try {
-                    Hyper-V\Optimize-VHD -Path $vhd.Path -Mode Full
-                    Write-Host ("Compacting VHDX") -ForegroundColor Green
-                }
-                catch {
-                    Write-Warning ("Error compacting {0}, dismounting..." -f $vhd.Path)
-                    Hyper-V\Dismount-VHD $vhd.Path
-                    return
-                }
-
-                try { 
-                    Hyper-V\Dismount-VHD $vhd.Path -ErrorAction Stop
-                    Write-Host ("Dismounting VHDX`n") -ForegroundColor Green
-                }
-                catch {
-                    Write-Warning ("Error dismounting {0}, please check Disk Management and manually dismount..." -f $vhd.Path)
-                    return
-                }        
+            Write-Host ("`nProcessing {0} from VM {1}..." -f $vhd.Path, $vhd.VMName) -ForegroundColor Gray
+            try {
+                Hyper-V\Mount-VHD -Path $vhd.Path -ReadOnly -ErrorAction Stop
+                Write-Host "Mounting VHDX" -ForegroundColor Green
             }
+            catch {
+                Write-Warning ("Error mounting {0}, please check access or if file is locked..." -f $vhd.Path )
+                continue
+            }
+
+            try {
+                Hyper-V\Optimize-VHD -Path $vhd.Path -Mode Full
+                Write-Host ("Compacting VHDX") -ForegroundColor Green
+            }
+            catch {
+                Write-Warning ("Error compacting {0}, dismounting..." -f $vhd.Path)
+                Hyper-V\Dismount-VHD $vhd.Path
+                return
+            }
+
+            try { 
+                Hyper-V\Dismount-VHD $vhd.Path -ErrorAction Stop
+                Write-Host ("Dismounting VHDX`n") -ForegroundColor Green
+            }
+            catch {
+                Write-Warning ("Error dismounting {0}, please check Disk Management and manually dismount..." -f $vhd.Path)
+                return
+            }        
         }
         else {
             Write-Warning ("VM {0} is Running, skipping..." -f $vhd.VMName)
