076936125226aa72cdb30c51f34f28611a22ddc9
Harm Veenstra <Harm.Veenstra@Gmail.com>
2024-11-25 20:18:39 +0100

    Made it more robust, skips items but continues if others succeed in submitting, changed default time-out to 45 seconds

--- Check if URL is safe/Test-URL.ps1
+++ Check if URL is safe/Test-URL.ps1
@@ -1,28 +1,28 @@
 param (
     [Parameter(Mandatory = $false)][string]$APIKEY = 'XXX-XXX-XXX',
-    [Parameter(Mandatory = $false)][int]$Seconds = 30,
+    [Parameter(Mandatory = $false)][int]$Seconds = 45,
     [Parameter(Mandatory = $true)][string[]]$URL,
     [Parameter(Mandatory = $false)][validateset("public", "unlisted", "private")][string]$Type = 'private'
 )
 
 #Retrieve actual url(s) for $URL
-try {
-    $ProgressPreference = "SilentlyContinue"
-    $ExpandedURLs = foreach ($Site in $URL) {
+$ProgressPreference = "SilentlyContinue"
+$ExpandedURLs = foreach ($Site in $URL) {
+    try {
+        $LongURL = (Invoke-WebRequest -Uri $Site -MaximumRedirection 0 -ErrorAction SilentlyContinue).Headers.Location
         [PSCustomObject]@{
             ShortURL = $Site
-            LongURL  = (Invoke-WebRequest -Uri $Site -MaximumRedirection 0 -ErrorAction SilentlyContinue).Headers.Location
+            LongURL  = if ($LongURL) { $LongURL } else { $site }
         }
         Write-Host ("Processed {0}..." -f $Site ) -ForegroundColor Green
     }
-}
-catch {
-    Write-Warning ("Specified {0} URL could not be found or expanded, exiting..." -f $URL)
-    return
+    catch {
+        Write-Warning ("Specified {0} URL could not be found or expanded, skipping..." -f $Site)
+    }
 }
 
 #Submit expanded $URL(s) for checking on urlscan.io if $ExpandedURLs returned LongURL(s)
-if ($null -ne $ExpandedURLs) {
+if ($ExpandedURLs) {
     $Headers = @{
         'Content-Type' = "application/json"
         'API-Key'      = "$APIKEY"
@@ -43,30 +43,39 @@ if ($null -ne $ExpandedURLs) {
             Write-Host ("Submitted specified {0} Short URL to be scanned as {1}..." -f $ExpandedURL.ShortURL, $ExpandedURL.LongURL) -ForegroundColor Green
         }
         catch {
-            Write-Warning ("Error submitting {0}, check URL manually on urlscan.io because it might be blocked from scanning)..." -f $ExpandedURL.ShortURL)
-            return
+            Write-Warning ("Error submitting {0}, check URL manually on urlscan.io because it might be blocked from scanning..." -f $ExpandedURL.ShortURL)
         }
     }
 
-    #Retrieve results for submitted urls, wait for the amount of seconds specified in $Seconds (Default is 30)
-    Write-Host ("Sleeping for {0} seconds to wait on results..." -f $Seconds) -ForegroundColor Green
-    Start-Sleep -Seconds $Seconds
-    $results = foreach ($response in $submits) {
-        try {
-            $data = Invoke-RestMethod -Uri $response.api -Method Get -ErrorAction Stop
-            [PSCustomObject]@{
-                ShortURL    = $response.ShortURL
-                LongURL     = $response.LongURL
-                Score       = $data.verdicts.overall.score
-                Categories  = if ($data.verdicts.overall.categories) { $data.verdicts.overall.categories } else { "None" }
-                Brands      = if ($data.verdicts.overall.brands) { $data.verdicts.overall.brands } else { "None" }
-                Malicious   = if ($data.verdicts.overall.Malicious) { $data.verdicts.overall.Malicious } else { "None" }
-                Hasverdicts = $data.verdicts.overall.hasVerdicts
+    #Retrieve results for submitted urls if any, wait for the amount of seconds specified in $Seconds (Default is 45)
+    if ($submits) {
+        Write-Host ("Sleeping for {0} seconds to wait on results..." -f $Seconds) -ForegroundColor Green
+        Start-Sleep -Seconds $Seconds
+        $results = foreach ($response in $submits) {
+            try {
+                $data = Invoke-RestMethod -Uri $response.api -Method Get -ErrorAction Stop
+                [PSCustomObject]@{
+                    ShortURL    = $response.ShortURL
+                    LongURL     = $response.LongURL
+                    Score       = $data.verdicts.overall.score
+                    Categories  = if ($data.verdicts.overall.categories) { $data.verdicts.overall.categories } else { "None" }
+                    Brands      = if ($data.verdicts.overall.brands) { $data.verdicts.overall.brands } else { "None" }
+                    Malicious   = if ($data.verdicts.overall.Malicious) { $data.verdicts.overall.Malicious } else { "None" }
+                    Hasverdicts = $data.verdicts.overall.hasVerdicts
+                }
+            }
+            catch {
+                Write-Warning ("Could not retrieve results for {0}" -f $response.ShortURL)
             }
         }
-        catch {
-            Write-Warning ("Could not retrieve results for {0}" -f $response.ShortURL)
-        }
+        return $results | Sort-Object ShortURL | Format-Table -AutoSize
     }
-    return $results | Sort-Object ShortURL | Format-Table -AutoSize
+    else {
+        Write-Warning ("URLS failed to submit, try to increase time-out or scan manually on urlscan.io. Exiting...")
+        return
+    }
+}
+else {
+    Write-Warning ("Supplied URL(s) could not be found, exiting..." )
+    return
 }
