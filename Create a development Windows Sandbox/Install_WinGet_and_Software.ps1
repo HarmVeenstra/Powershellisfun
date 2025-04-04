6e91eca8219c7eadc7a1e4917a82db679c87644c
Harm Veenstra <Harm.Veenstra@Gmail.com>
2025-04-04 22:06:31 +0200

    Added -Latest parameter to fix installation issue using Repar-WinGetPackageManager

--- Create a development Windows Sandbox/Install_WinGet_and_Software.ps1
+++ Create a development Windows Sandbox/Install_WinGet_and_Software.ps1
@@ -5,7 +5,7 @@ Write-Information "Downloading WinGet and its dependencies..."
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force:$true -Verbose
 Install-Module Microsoft.WinGet.Client -Force:$true -Confirm:$false -Verbose
 Import-Module Microsoft.WinGet.Client -Verbose
-Repair-WinGetPackageManager -Force:$true -Verbose
+Repair-WinGetPackageManager -Force:$true -Latest -Verbose
 
 #Install software
 $SoftwareToInstall = "Notepad++.Notepad++", "Microsoft.VisualStudioCode"
