0df8cf642b58f2bcafe21b269f6786adb563f697
Harm Veenstra <Harm.Veenstra@Inspark.nl>
2026-01-29 14:39:33 +0100

    Updated Detection to exit 1 if only one check fails

--- Wi-Fi 24H2 Remediation/Detection.ps1
+++ Wi-Fi 24H2 Remediation/Detection.ps1
@@ -1,25 +1,25 @@
 #Check Registry
 if ((Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\Wcmsvc | Select-Object -ExpandProperty DependOnService) -contains "WinHTTPAutoProxySvc") {
     Write-Output "WinHTTPAutoProxySvc key found in HKLM:\SYSTEM\CurrentControlSet\Services\Wcmsvc, needs Remediation"
-    $remediation = $true
+    $remediationdepeondson = $true
 }
 else {
     Write-Output "WinHTTPAutoProxySvc key not found in HKLM:\SYSTEM\CurrentControlSet\Services\Wcmsvc, no need for Remediation"
-    $remediation = $false
+    $remediationdepeondson = $false
 }
 
 #check service
 if ((Get-Service -Name WinHttpAutoProxySvc).StartType -ne 'Manual') {
     Write-Output "WinHTTP Web Proxy Auto-Discovery Service not configured as Manual, needs Remediation"
-    $remediation = $true
+    $remediationstarttype = $true
 }
 else {
     Write-Output "WinHTTP Web Proxy Auto-Discovery Service configured as Manual, no need for Remediation"
-    $remediation = $false
+    $remediationstarttype = $false
 }
 
 #exit with correct exit code
-if ($remediation) {
+if ($remediationdepeondson -or $remediationstarttype) {
     exit 1
 }
 else {
