24a0b94f8e76c0e0647481a0cf29bd5c0f162513
Harm Veenstra <Harm.Veenstra@Inspark.nl>
2026-04-13 20:32:12 +0200

    Moved $wshell = New-Object -ComObject wscript.shell to top of script for performance

--- Keep Microsoft Teams status on Available/Stop-MicrosoftTeamsAwayStatus.ps1
+++ Keep Microsoft Teams status on Available/Stop-MicrosoftTeamsAwayStatus.ps1
@@ -1,14 +1,14 @@
+$wshell = New-Object -ComObject wscript.shell
 while ($true) {
     try {
         Get-Process -Name 'ms-teams' -ErrorAction stop | Out-Null
-        Write-Host ("{0} - Microsoft Teams is running..." -f $(get-date)) -ForegroundColor Green
-        $wshell = New-Object -ComObject wscript.shell
+        Write-Host ("{0} - Microsoft Teams is running..." -f $(Get-Date)) -ForegroundColor Green
         $wshell.sendkeys("{NUMLOCK}{NUMLOCK}")
-        Write-Host ("{0} - Pressed NUMLOCK twice and waiting for 60 seconds" -f $(get-date)) -ForegroundColor Green
+        Write-Host ("{0} - Pressed NUMLOCK twice and waiting for 60 seconds" -f $(Get-Date)) -ForegroundColor Green
         Start-Sleep -Seconds 60          
     }
     catch {
-        Write-Warning ("{0} - Microsoft Teams is not running, sleeping for 15 seconds..." -f $(get-date))
+        Write-Warning ("{0} - Microsoft Teams is not running, sleeping for 15 seconds..." -f $(Get-Date))
         Start-Sleep -Seconds 15
     }
 }
